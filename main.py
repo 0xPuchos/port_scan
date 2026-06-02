32174449a61cf634fdee5045727c335bb4a18a1e
0xPuch <0xpuchos@proton.me>
2026-06-01 19:31:32 -0700

    Created working port scanner with python socket lib

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from port-scan!")
+import socket
+import threading
 
+def is_port_open(host: str, port: int):
+    """Check if the 'host' has any 'port' open """
+
+    # socket creation
+    my_socket = socket.socket()
+
+    try:
+        # attempts to connect to host using that port aka an address(a machine(host) and a specific door into that machine(port)
+        my_socket.connect((host, port)) 
+    except (ConnectionRefusedError, TimeoutError, OSError):
+        # cannot connect, port is closed
+        return False
+    else:
+        # attempt successful, port is open.
+        return True
+
+
+def main():
+    host: str = input("Enter the host: ")
+
+    for port in range(1,65536):
+        if is_port_open(host, port):
+            print(f"{host}:{port} is open")
 
 if __name__ == "__main__":
     main()
